ebce9ecd762131a5ad1662cca4d2d5fa44fc20ab
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-03-17 10:14:40 +0200

    refactored js
    extracted lots of methods
    fixed table cleanup

--- src/IOUService/web/ymmd.js
+++ src/IOUService/web/ymmd.js
@@ -1,15 +1,23 @@
 ﻿window.onload = function () {
+    set_up_send_button();
+    load_current_user();
+    load_sent_notes();
+}
+
+function set_up_send_button() {
     $("#thank_you_button").click(function () {
         $.ajax({
             url: "/api/IOU/SendThankYouNoteTo",
             type: "GET",
-            data: { recipient: $("#recipient").val()},
+            data: { recipient: $("#recipient").val() },
             success: function (result) {
-                reload_sent_notes();
+                load_sent_notes();
             }
         });
     });
+}
 
+function load_current_user() {
     $.ajax({
         url: "/api/IOU/GetCurrentUserName",
         type: "GET",
@@ -17,27 +25,31 @@
             $("#username").text(result);
         }
     });
-
-    reload_sent_notes();
 }
 
-function reload_sent_notes() {
+function load_sent_notes() {
     $.ajax({
         url: "/api/IOU/GetNotesSentByMe",
         type: "GET",
         success: function (result) {
-            console.log(result);
-            var table = document.getElementById("sent_notes_table"); // $("#sent_notes_table");
-            $("#sent_notes_table > tbody > tr").remove();
-
-            $.each(result, function (index, value) {
-                console.log(value);
-                var row = table.insertRow(-1);
-                var recipientCell = row.insertCell(-1);
-                var messageCell = row.insertCell(-1);
-                recipientCell.innerText = value.Recipient;
-                messageCell.innerText = value.Text;
-            });
+            show_sent_notes(result);
         }
     });
+}
+
+function show_sent_notes(notes) {
+    clear_table();
+
+    var table = document.getElementById("sent_notes_table");
+    $.each(notes, function(index, value) {
+        var row = table.insertRow(-1);
+        var recipientCell = row.insertCell(-1);
+        var messageCell = row.insertCell(-1);
+        recipientCell.innerText = value.Recipient;
+        messageCell.innerText = value.Text;
+    });
+}
+
+function clear_table() {
+    $("#sent_notes_table tr").not(":first").remove();
 }
