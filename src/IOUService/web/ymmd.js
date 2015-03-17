08a3e4ca63b4b42f2170b9ac083809041899bfd0
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-03-17 09:58:00 +0200

    added note sending and reloading using jQuery

--- src/IOUService/web/ymmd.js
+++ src/IOUService/web/ymmd.js
@@ -1,1 +1,43 @@
-﻿console.log("Hello World!")
+﻿window.onload = function () {
+    $("#thank_you_button").click(function () {
+        $.ajax({
+            url: "/api/IOU/SendThankYouNoteTo",
+            type: "GET",
+            data: { recipient: $("#recipient").val()},
+            success: function (result) {
+                reload_sent_notes();
+            }
+        });
+    });
+
+    $.ajax({
+        url: "/api/IOU/GetCurrentUserName",
+        type: "GET",
+        success: function (result) {
+            $("#username").text(result);
+        }
+    });
+
+    reload_sent_notes();
+}
+
+function reload_sent_notes() {
+    $.ajax({
+        url: "/api/IOU/GetNotesSentByMe",
+        type: "GET",
+        success: function (result) {
+            console.log(result);
+            var table = document.getElementById("sent_notes_table"); // $("#sent_notes_table");
+            $("#sent_notes_table > tbody > tr").remove();
+
+            $.each(result, function (index, value) {
+                console.log(value);
+                var row = table.insertRow(-1);
+                var recipientCell = row.insertCell(-1);
+                var messageCell = row.insertCell(-1);
+                recipientCell.innerText = value.Recipient;
+                messageCell.innerText = value.Text;
+            });
+        }
+    });
+}
