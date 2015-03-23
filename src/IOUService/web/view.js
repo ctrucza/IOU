121378f9f25c0916a69b70996a5fd0c9f8a205a2
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-03-23 13:46:55 +0200

    extracted api and view to separate files

---
+++ src/IOUService/web/view.js
@@ -0,0 +1,11 @@
+﻿var view = {
+    show_current_user: function (user_name) {
+        $("#username").text(user_name);
+    },
+    show_sent_notes: function (notes) {
+        display_notes_table("sent_notes_table", notes);
+    },
+    show_received_notes: function (notes) {
+        display_notes_table("received_notes_table", notes);
+    }
+};
--- src/IOUService/web/ymmd.js
+++ src/IOUService/web/ymmd.js
@@ -1,72 +1,13 @@
-﻿var api = {
-    on_current_user_loaded: undefined,
-    load_current_user: function() {
-        $.ajax({
-            url: "/api/IOU/GetCurrentUserName",
-            type: "GET",
-            success: function (result) {
-                api.on_current_user_loaded(result);
-            }
-        });
-    },
-
-    on_sent_notes_loaded: undefined,
-    load_sent_notes: function() {
-        $.ajax({
-            url: "/api/IOU/GetNotesSentByMe",
-            type: "GET",
-            success: function (result) {
-                api.on_sent_notes_loaded(result);
-            }
-        });
-    },
-
-    on_received_notes_loaded: undefined,
-    load_received_notes: function() {
-        $.ajax({
-            url: "/api/IOU/GetMyNotes",
-            type: "GET",
-            success: function (result) {
-                api.on_received_notes_loaded(result);
-            }
-        });
-    },
-
-    send_note: function() {
-        $.ajax({
-            url: "/api/IOU/SendThankYouNoteTo",
-            type: "GET",
-            data: { recipient: $("#recipient").val() },
-            success: function () {
-                api.load_sent_notes();
-            }
-        });
-    }
-};
-
-var view = {
-    show_current_user: function(user_name) {
-        $("#username").text(user_name);
-    },
-    show_sent_notes: function(notes) {
-        display_notes_table("sent_notes_table", notes);
-    },
-    show_received_notes: function(notes) {
-        display_notes_table("received_notes_table", notes);
-    }
-};
-
-window.onload = function () {
+﻿window.onload = function () {
     set_up_send_button();
     set_up_refresh_button();
 
     api.on_current_user_loaded = view.show_current_user;
-    api.load_current_user();
-
     api.on_sent_notes_loaded = view.show_sent_notes;
-    api.load_sent_notes();
-
     api.on_received_notes_loaded = view.show_received_notes;
+
+    api.load_current_user();
+    api.load_sent_notes();
     api.load_received_notes();
 }
 
