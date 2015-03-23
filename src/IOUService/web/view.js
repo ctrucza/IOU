a37a016b697241c3951eab7a9a185812bf945593
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-03-23 13:54:11 +0200

    moved view-related code in the view class
    Including setting the delegate

--- src/IOUService/web/view.js
+++ src/IOUService/web/view.js
@@ -8,9 +8,48 @@
     show_received_notes: function (notes) {
         display_notes_table("received_notes_table", notes);
     },
-    set_delegate: function(delegate) {
+
+    delegate: undefined,
+    set_delegate: function (delegate) {
+        view.delegate = delegate;
         delegate.on_current_user_loaded = view.show_current_user;
         delegate.on_sent_notes_loaded = view.show_sent_notes;
         delegate.on_received_notes_loaded = view.show_received_notes;
+
+        set_up_send_button(delegate);
+        set_up_refresh_button(delegate);
+    },
+
+    refresh: function() {
+        view.delegate.load_current_user();
+        view.delegate.load_sent_notes();
+        view.delegate.load_received_notes();
     }
 };
+
+function set_up_send_button(delegate) {
+    $("#thank_you_button").click(function () {
+        delegate.send_note();
+    });
+}
+
+function set_up_refresh_button(delegate) {
+    $("#refresh_button").click(delegate.load_received_notes);
+};
+
+function display_notes_table(table_id, notes) {
+    clear_table(table_id);
+
+    var table = document.getElementById(table_id);
+    $.each(notes, function (index, value) {
+        var row = table.insertRow(-1);
+        var recipientCell = row.insertCell(-1);
+        var messageCell = row.insertCell(-1);
+        recipientCell.innerText = value.Recipient;
+        messageCell.innerText = value.Text;
+    });
+}
+
+function clear_table(table_id) {
+    $("#" + table_id + " tr").not(":first").remove();
+}
--- src/IOUService/web/ymmd.js
+++ src/IOUService/web/ymmd.js
@@ -1,37 +1,5 @@
 ﻿window.onload = function () {
-    set_up_send_button();
-    set_up_refresh_button();
-
     view.set_delegate(api);
-
-    api.load_current_user();
-    api.load_sent_notes();
-    api.load_received_notes();
+    view.refresh();
 }
 
-function set_up_send_button() {
-    $("#thank_you_button").click(function() {
-        api.send_note();
-    });
-}
-
-function set_up_refresh_button() {
-    $("#refresh_button").click(api.load_received_notes);
-};
-
-function display_notes_table(table_id, notes) {
-    clear_table(table_id);
-
-    var table = document.getElementById(table_id);
-    $.each(notes, function (index, value) {
-        var row = table.insertRow(-1);
-        var recipientCell = row.insertCell(-1);
-        var messageCell = row.insertCell(-1);
-        recipientCell.innerText = value.Recipient;
-        messageCell.innerText = value.Text;
-    });
-}
-
-function clear_table(table_id) {
-    $("#" + table_id + " tr").not(":first").remove();
-}
