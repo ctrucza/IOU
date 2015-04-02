f57eb66ff2e20c7656aaebca08af7cf47d5b945d
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-04-02 15:55:50 +0300

    extracted methods

--- src/IOUService/web/ymmd.js
+++ src/IOUService/web/ymmd.js
@@ -1,5 +1,10 @@
 ﻿window.onload = function () {
+    setup_view();
+    setup_api();
+    refresh_view();
+}
 
+function setup_view() {
     // wow! such complex!
     view.get_send_button().click(function () {
         api.send_note(view.get_recipient());
@@ -8,11 +13,15 @@
     view.get_refresh_button().click(function () {
         api.load_received_notes();
     });
+}
 
+function setup_api() {
     api.on_current_user_loaded = view.show_current_user_name;
     api.on_sent_notes_loaded = view.show_sent_notes;
     api.on_received_notes_loaded = view.show_received_notes;
+}
 
+function refresh_view() {
     // is our api coherent enough? This three lines look like they could go into the api.
     // but that would create ugliness there. Maybe we miss something?
     api.load_current_user();
