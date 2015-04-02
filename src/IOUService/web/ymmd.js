91b3a19f7b31b8519df2937cc4a8a81432848207
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-04-02 17:31:15 +0300

    extracted setup/init methods

--- src/IOUService/web/ymmd.js
+++ src/IOUService/web/ymmd.js
@@ -1,28 +1,30 @@
 ﻿window.onload = function () {
-    set_up_send_button();
-    set_up_refresh_button();
-
-    api.on_current_user_loaded = show_current_user_name;
-    api.on_sent_notes_loaded = view.show_sent_notes;
-    api.on_received_notes_loaded = view.show_received_notes;
-
-    api.load_current_user();
-    api.load_sent_notes();
-    api.load_received_notes();
+    setup_view();
+    setup_api();
+    refresh_view();
 }
 
 function show_current_user_name(current_user_name) {
+    view.current_username_label.text(current_user_name);
 }
 
-function set_up_send_button() {
+function setup_view() {
     view.send_button.click(function () {
         api.send_note(view.recipient_edit.val());
     });
-}
-
-function set_up_refresh_button() {
     view.refresh_button.click(function () {
         api.load_received_notes();
     });
 }
 
+function setup_api() {
+    api.on_current_user_loaded = show_current_user_name;
+    api.on_sent_notes_loaded = view.show_sent_notes;
+    api.on_received_notes_loaded = view.show_received_notes;
+}
+
+function refresh_view() {
+    api.load_current_user();
+    api.load_sent_notes();
+    api.load_received_notes();
+}
