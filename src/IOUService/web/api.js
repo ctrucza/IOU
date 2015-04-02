05eb675ae022f4fc5a739e9d23c2a63c0efa6a34
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-04-02 17:44:31 +0300

    refactored api: remove duplication
    move network access in own class

--- src/IOUService/web/api.js
+++ src/IOUService/web/api.js
@@ -1,42 +1,31 @@
 ﻿var api = {
-    // interface
     delegate: undefined,
     
     load_current_user: function () {
-        $.ajax({
-            url: "/api/IOU/GetCurrentUserName",
-            type: "GET",
-            success: function (result) {
-                api.delegate.on_current_user_loaded(result);
-            }
-        });
+        http.get("/api/IOU/GetCurrentUserName", api.delegate.on_current_user_loaded);
     },
+
     load_sent_notes: function () {
-        $.ajax({
-            url: "/api/IOU/GetNotesSentByMe",
-            type: "GET",
-            success: function (result) {
-                api.delegate.on_sent_notes_loaded(result);
-            }
-        });
+        http.get("/api/IOU/GetNotesSentByMe", api.delegate.on_sent_notes_loaded);
     },
+
     load_received_notes: function () {
-        $.ajax({
-            url: "/api/IOU/GetMyNotes",
-            type: "GET",
-            success: function (result) {
-                api.delegate.on_received_notes_loaded(result);
-            }
-        });
+        http.get("/api/IOU/GetMyNotes", api.delegate.on_received_notes_loaded);
     },
+
     send_note: function (recipient) {
-        $.ajax({
-            url: "/api/IOU/SendThankYouNoteTo",
-            type: "GET",
-            data: { recipient: recipient },
-            success: function () {
-                api.delegate.on_note_sent();
-            }
-        });
+        http.get("/api/IOU/SendThankYouNoteTo", api.delegate.on_note_sent, { recipient: recipient });
     }
 };
+
+var http = {
+    get: function (url, callback, data) {
+        $.ajax({
+            url: url,
+            type: "GET",
+            data: data,
+            success: callback
+        });
+
+    }
+}
