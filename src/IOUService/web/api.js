949f971c487e429c2c36d06a44c719b6e03e32ab
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-04-03 13:07:34 +0300

    faked http
    integration tests now can run woth the fake api. They still need to run
    against the real web app though. But it paves the way for real unit
    tests to be written

--- src/IOUService/web/api.js
+++ src/IOUService/web/api.js
@@ -1,4 +1,36 @@
-﻿var api = {
+﻿var real_http = {
+    get: function (url, callback, data) {
+        $.ajax({
+            url: url,
+            type: "GET",
+            data: data,
+            success: callback
+        });
+
+    }
+}
+
+var fake_http = {
+    sent_notes: [],
+    received_notes:[],
+    get: function(url, callback, data) {
+        if (url === "/api/IOU/GetCurrentUserName") {
+            callback("John Doe");
+        } else if (url === "/api/IOU/GetNotesSentByMe") {
+            callback(fake_http.sent_notes);
+        } else if (url === "/api/IOU/GetMyNotes") {
+            callback(fake_http.received_notes);
+        } else if (url === "/api/IOU/SendThankYouNoteTo") {
+            fake_http.sent_notes.push({ Sender: "John Doe", Recipient: data.recipient, Text: "Thank you, Jane Doe! Sincerely, John Doe" });
+            fake_http.received_notes.push({ Sender: data.recipient, Recipient: "John Doe", Text: "Thank you, John Doe! Sincerely, Jane Doe" });
+            callback();
+        }
+    }
+}
+
+var http = fake_http;
+
+var api = {
     delegate: undefined,
     
     load_current_user: function () {
@@ -17,15 +49,3 @@
         http.get("/api/IOU/SendThankYouNoteTo", api.delegate.on_note_sent, { recipient: recipient });
     }
 };
-
-var http = {
-    get: function (url, callback, data) {
-        $.ajax({
-            url: url,
-            type: "GET",
-            data: data,
-            success: callback
-        });
-
-    }
-}
