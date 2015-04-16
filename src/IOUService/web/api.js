79c792caa7415393693586d8d6000d7862257acc
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-04-16 11:46:25 +0300

    objectified fake http

--- src/IOUService/web/api.js
+++ src/IOUService/web/api.js
@@ -1,18 +1,23 @@
 ﻿function FakeHttp() {
-    this.sent_notes = [];
-    this.received_notes = [];
-    this.get = function (url, callback, data) {
+    var sent_notes = [];
+    var received_notes = [];
+
+    function get(url, callback, data) {
         if (url === "/api/IOU/GetCurrentUserName") {
             callback("John Doe");
         } else if (url === "/api/IOU/GetNotesSentByMe") {
-            callback(this.sent_notes);
+            callback(sent_notes);
         } else if (url === "/api/IOU/GetMyNotes") {
-            callback(this.received_notes);
+            callback(received_notes);
         } else if (url === "/api/IOU/SendThankYouNoteTo") {
-            this.sent_notes.push({ Sender: "John Doe", Recipient: data.recipient, Text: "Thank you, Jane Doe! Sincerely, John Doe" });
-            this.received_notes.push({ Sender: data.recipient, Recipient: "John Doe", Text: "Thank you, John Doe! Sincerely, Jane Doe" });
+            sent_notes.push({ Sender: "John Doe", Recipient: data.recipient, Text: "Thank you, Jane Doe! Sincerely, John Doe" });
+            received_notes.push({ Sender: data.recipient, Recipient: "John Doe", Text: "Thank you, John Doe! Sincerely, Jane Doe" });
             callback();
         }
+    }
+
+    return {
+        get: get
     }
 }
 
