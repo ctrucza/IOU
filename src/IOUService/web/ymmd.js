480d965b69d36e38fd6b2a7399245c54c6783192
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-04-28 17:26:46 +0300

    added dropdown for recipient

--- src/IOUService/web/ymmd.js
+++ src/IOUService/web/ymmd.js
@@ -2,4 +2,33 @@
     var controller = new Controller(new View(), new Api());
 
     controller.refresh_ui();
+
+    var availableTags = [
+      "ActionScript",
+      "AppleScript",
+      "Asp",
+      "BASIC",
+      "C",
+      "C++",
+      "Clojure",
+      "COBOL",
+      "ColdFusion",
+      "Erlang",
+      "Fortran",
+      "Groovy",
+      "Haskell",
+      "Java",
+      "JavaScript",
+      "Lisp",
+      "Perl",
+      "PHP",
+      "Python",
+      "Ruby",
+      "Scala",
+      "Scheme"
+    ];
+
+    $("#recipient").autocomplete({
+        source: availableTags
+    });
 };
