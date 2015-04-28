1d9c89e792a228306d66ee85693a381cca86eef1
Csaba Trucza <csaba.trucza@iquestgroup.com>
2015-04-28 18:03:31 +0300

    cleanup

--- src/IOUService/web/ymmd.js
+++ src/IOUService/web/ymmd.js
@@ -3,31 +3,6 @@
 
     controller.refresh_ui();
 
-    var availableTags = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
-    ];
-
     $("#recipient").autocomplete({
         source: function (request, response) {
             var search_term = request.term;
